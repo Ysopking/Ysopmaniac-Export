3891f0ef1a7c413d31fe3cb049ecd0fce134c6ad
Ysop1996 <114836027+Ysop1996@users.noreply.github.com>
2026-05-02 06:29:18 +0200

    Fix: BiometricType-Import in passkey_setup_screen ergaenzt
    local_auth-Import fehlte, dadurch konnte das Enum BiometricType
    im _typeLabel() nicht aufgeloest werden -> kernel_snapshot Fehler.

--- lib/screens/passkey_setup_screen.dart
+++ lib/screens/passkey_setup_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:local_auth/local_auth.dart';
 import 'package:url_launcher/url_launcher.dart';
 import '../logic/state_provider.dart';
 import '../services/security_service.dart';
